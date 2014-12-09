1bcf120fcf0fcc9db6cfffb419fa3f478bf9c66a
Robert Kaye <rob@musicbrainz.org>
2014-12-09 20:12:15 +0000

    Added XYZ calculations in arduino code with sin/cos table lookups.

--- arduino/data-poi/data-poi.ino
+++ arduino/data-poi/data-poi.ino
@@ -1,38 +1,92 @@
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
-
 #include <Wire.h>
+#include "sin.h"
 
 uint8_t red_pin = 10;
 uint8_t blue_pin = 9;
 uint8_t green_pin = 11;
 
-float angles[3]; // yaw pitch roll
-float eu_angles[3];
+const float TWOPI = M_PI * 2.0;
 
 // Set the FreeSixIMU object
 FreeSixIMU sixDOF = FreeSixIMU();
 
+void led_color(uint8_t r, uint8_t g, uint8_t b)
+{
+    analogWrite(red_pin, r);
+    analogWrite(green_pin, g);
+    analogWrite(blue_pin, b);    
+}
+
+void startup(void)
+{
+    uint8_t i;
+    uint8_t col1[3] = { 128, 70, 0 };
+    uint8_t col2[3] = { 128, 0, 128 };
+    
+    for(i = 0; i < 10; i++)
+    {
+        led_color(128, 70, 0);
+        delay(100);
+        led_color(128, 0, 128);
+        delay(100);
+    }    
+    led_color(0, 0, 0);    
+}
+
+float sin(float value)
+{
+    float rad_value;
+    uint8_t index;
+    
+    rad_value = value / TWO_PI;
+    index = (int)(rad_value / sin_table_entries);
+    return pgm_read_float(&(sin_table[index]));
+}
+
+float cos(float value)
+{
+    float rad_value;
+    uint8_t index;
+    
+    rad_value = value / TWO_PI;
+    index = (int)(rad_value / sin_table_entries);
+    return pgm_read_float(&(cos_table[index]));
+}
+
 void setup() 
 { 
+    startup();
+    
     Serial.begin(38400);
     Wire.begin();
 
     delay(5);
-    sixDOF.init(); //begin the IMU
+    sixDOF.init();
     delay(5);
 }
 
 void loop() 
 { 
     uint8_t i; 
+    float x, y, z;
+    float angles[3]; // yaw pitch roll
+    float theta, phi;
 
-    sixDOF.getEuler(eu_angles);
-
-    Serial.print(int(eu_angles[0]));
+    sixDOF.getEuler(angles);
+    
+    theta = angles[2] / TWO_PI;
+    phi = angles[1] / TWO_PI;
+    
+    x = sin(theta) * cos(phi);
+    y = sin(theta) * sin(phi);
+    z = cos(theta);
+    
+    Serial.print(int(angles[0]));
     Serial.print(",");  
-    Serial.print(int(eu_angles[1]));
+    Serial.print(int(angles[1]));
     Serial.print(",");
-    Serial.println(int(eu_angles[2]));    
+    Serial.println(int(angles[2]));    
 }
