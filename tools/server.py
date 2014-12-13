a3c40650071ce35d312f486a3f6744c7fe321b26
Robert Kaye <rob@musicbrainz.org>
2014-12-13 14:07:21 +0000

    Implemented OSC sending

---
+++ tools/server.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import serial
+import time
+import math
+import argparse
+import OSC
+
+BAUD_RATE       = 38400
+
+def send_osc(ip, port, index, key, value):
+    msg = OSC.OSCMessage()
+    msg.setAddress("/rpoi-%d/%s" % (index, key))
+    msg.append(value)
+    client.sendto(msg, (ip, port))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip",
+                    default="127.0.0.1", help="The ip to broadcast to")
+parser.add_argument("--port",
+                    type=int, default=5000, help="The port to broadcast to")
+parser.add_argument("--device",
+                    default="", help="The serial device to read poi data from")
+args = parser.parse_args()
+
+if args.device == "":
+    print "Must specify --device"
+    sys.exit(-1)
+
+print "Listening from %s" % args.device
+print "Sending to %s:%d" % (args.ip, args.port)
+
+client = OSC.OSCClient()
+
+try:
+    print "Connecting to Poi..."
+    ser = serial.Serial(args.device, 
+                        BAUD_RATE, 
+                        bytesize=serial.EIGHTBITS, 
+                        parity=serial.PARITY_NONE, 
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1)
+except serial.serialutil.SerialException, e:
+    print "Serial error: ", e
+    sys.exit(-1)
+except OSError, e:
+    print "OS error: ", e
+    sys.exit(-1)
+
+print "connected."
+
+ser.flushInput()
+ser.flushOutput()
+
+line = ""
+while True:
+    ch = ser.read()
+    if not ch:
+        print "Timeout"
+        continue
+
+    if ch != '\n':
+        line += ch
+        continue
+
+    try:
+        ts, yaw, pitch, roll = line.strip().split(",")
+    except ValueError as e:
+        continue
+
+    line  = ""
+
+    theta = math.radians(float(roll))
+    phi = math.radians(float(pitch))
+    x = math.sin(theta) * math.cos(phi)
+    y = math.sin(theta) * math.sin(phi)
+    z = math.cos(theta)
+#    print "%s,%.3f,%.3f,%.3f" % (ts, x, y, z)
+
+    send_osc(args.ip, args.port, 0, "x", x)
+    send_osc(args.ip, args.port, 0, "y", y)
+    send_osc(args.ip, args.port, 0, "z", z)
+    send_osc(args.ip, args.port, 0, "ts", ts)
+
+ser.close()
+
