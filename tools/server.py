bd8333d1e045a8d97d32e50d30ae548f86947731
Robert Kaye <rob@musicbrainz.org>
2014-12-13 15:00:15 +0000

    Restructured and added test mode

--- tools/server.py
+++ tools/server.py
@@ -8,7 +8,7 @@ import math
 import argparse
 import OSC
 
-BAUD_RATE       = 38400
+BAUD_RATE = 38400
 
 def send_osc(ip, port, index, key, value):
     msg = OSC.OSCMessage()
@@ -16,73 +16,82 @@ def send_osc(ip, port, index, key, value):
     msg.append(value)
     client.sendto(msg, (ip, port))
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip",
-                    default="127.0.0.1", help="The ip to broadcast to")
-parser.add_argument("--port",
-                    type=int, default=9000, help="The port to broadcast to")
-parser.add_argument("--device",
-                    default="", help="The serial device to read poi data from")
-args = parser.parse_args()
+def main_loop(ser, client, args):
+    line = ""
+    while True:
+        ch = ser.read()
+        if not ch:
+            print "Timeout"
+            continue
 
-if args.device == "":
-    print "Must specify --device"
-    sys.exit(-1)
+        if ch != '\n':
+            line += ch
+            continue
 
-print "Listening from %s" % args.device
-print "Sending to %s:%d" % (args.ip, args.port)
+        try:
+            ts, yaw, pitch, roll = line.strip().split(",")
+        except ValueError as e:
+            continue
 
-client = OSC.OSCClient()
+        line  = ""
 
-try:
-    print "Connecting to Poi..."
-    ser = serial.Serial(args.device, 
-                        BAUD_RATE, 
-                        bytesize=serial.EIGHTBITS, 
-                        parity=serial.PARITY_NONE, 
-                        stopbits=serial.STOPBITS_ONE,
-                        timeout=1)
-except serial.serialutil.SerialException, e:
-    print "Serial error: ", e
-    sys.exit(-1)
-except OSError, e:
-    print "OS error: ", e
-    sys.exit(-1)
+        theta = math.radians(float(roll))
+        phi = math.radians(float(pitch))
+        x = math.sin(theta) * math.cos(phi)
+        y = math.sin(theta) * math.sin(phi)
+        z = math.cos(theta)
 
-print "connected."
+        if args.test:
+            print "%s,%.3f,%.3f,%.3f" % (ts, x, y, z)
+        else:
+            send_osc(args.ip, args.port, 0, "x", x)
+            send_osc(args.ip, args.port, 0, "y", y)
+            send_osc(args.ip, args.port, 0, "z", z)
+            send_osc(args.ip, args.port, 0, "ts", ts)
 
-ser.flushInput()
-ser.flushOutput()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action='store_true',
+                        default=False, help="Instead of broadcasting, print data to console")
+    parser.add_argument("--ip",
+                        default="127.0.0.1", help="The ip to broadcast to")
+    parser.add_argument("--port",
+                        type=int, default=9000, help="The port to broadcast to")
+    parser.add_argument("--device",
+                        default="", help="The serial device to read poi data from")
+    args = parser.parse_args()
 
-line = ""
-while True:
-    ch = ser.read()
-    if not ch:
-        print "Timeout"
-        continue
+    if args.device == "":
+        print "Must specify --device"
+        sys.exit(-1)
 
-    if ch != '\n':
-        line += ch
-        continue
+    print "Listening from %s" % args.device
+    print "Sending to %s:%d" % (args.ip, args.port)
+
+    client = OSC.OSCClient()
 
     try:
-        ts, yaw, pitch, roll = line.strip().split(",")
-    except ValueError as e:
-        continue
+        print "Connecting to Poi..."
+        ser = serial.Serial(args.device, 
+                            BAUD_RATE, 
+                            bytesize=serial.EIGHTBITS, 
+                            parity=serial.PARITY_NONE, 
+                            stopbits=serial.STOPBITS_ONE,
+                            timeout=1)
+    except serial.serialutil.SerialException, e:
+        print "Serial error: ", e
+        sys.exit(-1)
+    except OSError, e:
+        print "OS error: ", e
+        sys.exit(-1)
 
-    line  = ""
+    print "connected."
 
-    theta = math.radians(float(roll))
-    phi = math.radians(float(pitch))
-    x = math.sin(theta) * math.cos(phi)
-    y = math.sin(theta) * math.sin(phi)
-    z = math.cos(theta)
-#    print "%s,%.3f,%.3f,%.3f" % (ts, x, y, z)
+    ser.flushInput()
+    ser.flushOutput()
 
-    send_osc(args.ip, args.port, 0, "x", x)
-    send_osc(args.ip, args.port, 0, "y", y)
-    send_osc(args.ip, args.port, 0, "z", z)
-    send_osc(args.ip, args.port, 0, "ts", ts)
-
-ser.close()
-
+    try:
+        main_loop(ser, client, args)
+    except KeyboardInterrupt:
+        print "cleaning up..."
+        ser.close(ser)
